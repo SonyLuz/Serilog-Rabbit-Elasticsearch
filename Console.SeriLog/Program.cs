84c5fbfcb7bc4474b73f19d76c4bea0dbe9c75f8
Sony Luz <sonyluz@bne.com.br>
2020-10-07 10:01:19 -0300

    first commit

--- Console.SeriLog/Program.cs
+++ Console.SeriLog/Program.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
 using Serilog;
 using System;
 using System.IO;
+using Serilog.Sinks.RabbitMQ;
+using Serilog.Sinks.RabbitMQ.Sinks.RabbitMQ;
+using Serilog.Formatting.Json;
 
 namespace Console.SeriLog
 {
@@ -10,14 +12,41 @@ namespace Console.SeriLog
     {
         static void Main(string[] args)
         {
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
             var configurationBuilder = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("Serilog.json", optional: false, reloadOnChange: true);
 
             var configuration = configurationBuilder.Build();
 
+            var configClient = new RabbitMQClientConfiguration
+            {
+                Username = "rabbitmq",
+                Password = "rabbitmq",
+                Exchange = "LogEvent",
+                ExchangeType = "fanout",
+                DeliveryMode = RabbitMQDeliveryMode.NonDurable,
+                RouteKey = "Logs",
+                Port = 5672,
+                VHost = "/"
+            };
+
+            configClient.Hostnames.Add("localhost");
+
+            RabbitMQSinkConfiguration sinkConfiguration = new RabbitMQSinkConfiguration
+            {
+                BatchPostingLimit = 0,
+                Period = default,
+                TextFormatter = new JsonFormatter()
+            };
+
+
             Log.Logger = new LoggerConfiguration()
+                .WriteTo.RabbitMQ(configClient, sinkConfiguration)
                 .ReadFrom.Configuration(configuration)
+                .Enrich.WithProperty("ApplicationName", "Console.SeriLog")
+                .Enrich.WithProperty("Topic", $"Console.SeriLog-{environment}")
                 .CreateLogger();
 
             try
@@ -27,6 +56,7 @@ namespace Console.SeriLog
                 Log.Warning("Warning - Log warning");
                 Log.Error("Error - Log error");
                 Log.Fatal("Fatal - Log fatal");
+                Log.Fatal(new Exception("teste"), "Fatal - Log fatal");
             }
             catch (Exception ex)
             {
